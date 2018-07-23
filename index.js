c78644f953f2f808d55099790481d41b1a7a0110
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-23 12:28:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ var recipes = {}
 function updateObjectWithKeyAndValue (object, key, value) {
   let tempObj = {...object}
   tempObj[key] = value
+  return tempObj
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
